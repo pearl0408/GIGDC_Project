4ef928180c51db20adefc0b9bbebca110fdf3437
stopkite <62979643+stopkite@users.noreply.github.com>
2021-06-30 16:05:53 +0900

    개강 일주일전 메시지 기능 구현

--- Assets/Script/SendMsgTCousin.cs
+++ Assets/Script/SendMsgTCousin.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ShowUpMsg : MonoBehaviour
+public class SendMsgTCousin : MonoBehaviour
 {
     public Button touchPanel;
     public int Cnt;
