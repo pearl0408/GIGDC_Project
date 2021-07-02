12d300d9165ac8573f9709118b9474035ea25fb7
김채영 <iiblueblue@naver.com>
2021-07-02 17:57:56 +0900

    Phone 프리팹 수정, kcy 씬에 알림센터 구현(이미지는 아직)

--- Assets/Scripts/Script_Drag/DragNotice.cs
+++ Assets/Scripts/Script_Drag/DragNotice.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.EventSystems;
 
 public class DragNotice : MonoBehaviour
 {
@@ -18,32 +17,31 @@ public class DragNotice : MonoBehaviour
     void Update()
     {
         if(Input.GetMouseButtonDown(0))
-        {
-            Debug.Log(startPos);
+        {           
             this.startPos = Input.mousePosition;
         }
         else if(Input.GetMouseButtonUp(0))
         {
             Vector2 endPos = Input.mousePosition;
-            float swipeLength = endPos.y - this.startPos.y;
-            Debug.Log(endPos);
+            float swipeLength = endPos.y - this.startPos.y;           
 
-            if (swipeLength > 5)
+            //위로 스와이프
+            if (startPos.y > 299 && startPos.y < 330 && swipeLength > 10 && transform.position.y < 1000)
             {
                 StartCoroutine(swipe(1));
-                Debug.Log("5보다 큼");
+                
             }
-            else if (swipeLength < -5)
+            //아래로 스와이프
+            else if (startPos.y > 1592 && startPos.y < 1660 && swipeLength < -10 && transform.position.y > 1000)
             {
-                StartCoroutine(swipe(-1));
-                Debug.Log("5보다 작음");
+                StartCoroutine(swipe(-1));               
             }
         }
     }
 
     IEnumerator swipe(int j)
     {
-        for(int i=0; i<50; i++)
+        for(int i=0; i<30; i++)
         {
             transform.Translate(0, 50*j, 0);
             yield return new WaitForSeconds(0.01f); //0.01초 딜레이
