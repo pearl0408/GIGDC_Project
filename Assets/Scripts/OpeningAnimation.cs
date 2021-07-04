a1ed4e96af70f88dea61668962e2a37bec177c15
김채영 <iiblueblue@naver.com>
2021-07-04 19:50:54 +0900

    kcy에서 시계돌리는 애니메이션 시도 중

--- Assets/Scripts/OpeningAnimation.cs
+++ Assets/Scripts/OpeningAnimation.cs
@@ -8,6 +8,7 @@ using UnityEngine.UI;
 public class OpeningAnimation : MonoBehaviour, IDragHandler, IEndDragHandler
 {
     private float speed = 3f;
+    public GameObject minute;
 
     // Start is called before the first frame update
     void Start()
@@ -24,12 +25,12 @@ public class OpeningAnimation : MonoBehaviour, IDragHandler, IEndDragHandler
     void IDragHandler.OnDrag(PointerEventData eventData)
     {
         Debug.Log("들어옴");
-        transform.Rotate(0f, 0f, Input.GetAxis("Mouse X") * Input.GetAxis("Mouse Y") * speed);
+        //transform.Rotate(0f, 0f, Input.GetAxis("Mouse X") * Input.GetAxis("Mouse Y") * speed);
         //transform.Rotate(-Input.GetAxis("Mouse Y") , 0f, 0f);
 
-        //var dir = new Vector3(eventData.position.x, eventData.position.y, 0) - Camera.main.WorldToScreenPoint(transform.position);//마우스 방향 계산하기
-        //var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;//회전각 계산하기
-        //transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);//화살표 회전각 바꾸기
+        var dir = new Vector3(eventData.position.x, eventData.position.y, 0) - minute.transform.position;//마우스 방향 계산하기
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;//회전각 계산하기
+        minute.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);//화살표 회전각 바꾸기
     }
 
     void IEndDragHandler.OnEndDrag(PointerEventData eventData)
